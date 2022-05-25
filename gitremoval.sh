a66f8648ebdd17473c5ff74bb35115c87e2afaa7
Subhendu Pratihari <spratihari@altimetrik.com>
2022-05-25 10:21:16 +0100

    removaloffiles

--- gitremoval.sh
+++ gitremoval.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git log --after=" 1 days ago" | git branch --sort=committerdate | egrep -v "(^\\*|master|dev|test8)" | xargs git branch -D
+git log --after=" 1 days ago" | git branch --sort=committerdate | egrep -v "(^\\*|master|dev|test8)" | xargs git branch -d testbranch28
